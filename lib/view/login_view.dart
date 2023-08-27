e33185ca8f5e23fb886d50ccadaf5bdca95d3a38
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-08-27 23:10:59 +0300

    create login view

---
+++ lib/view/login_view.dart
@@ -0,0 +1,64 @@
+import 'package:chat_app/widget/box_text_field.dart';
+import 'package:chat_app/widget/main_button.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: const Color(0XFF2B475E),
+      body: Center(
+        child: SizedBox(
+          width: screenSize.width * 0.9,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/scholar.png',),
+              const Text(
+                'Scholar Chat',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+              SizedBox(height: screenSize.height * 0.05),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'SignIn',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 26,
+                  ),
+                ),
+              ),
+              SizedBox(height: screenSize.height * 0.03),
+              Form(
+                child: Column(
+                  children: [
+                    const BoxTextField(
+                      hintText: 'Email',
+                      obscureText: false,
+                    ),
+                    SizedBox(height: screenSize.height * 0.01),
+                    const BoxTextField(
+                      hintText: 'Password',
+                      obscureText: true,
+                    ),
+                    SizedBox(height: screenSize.height * 0.03),
+                    const MainButton(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
