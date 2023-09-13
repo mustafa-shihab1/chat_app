eaadf7a478c97c1a26fc49945d4e4814eef72325
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-09-13 22:43:12 +0300

    edit auth view

--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,100 +1,108 @@
+import 'package:chat_app/features/auth/controller/auth_controller.dart';
 import 'package:chat_app/features/auth/widget/box_text_field.dart';
 import 'package:chat_app/features/auth/widget/main_button.dart';
+import 'package:chat_app/routes/routes.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String? email;
-    String? password;
     Size screenSize = MediaQuery.of(context).size;
-    return Scaffold(
-      backgroundColor: const Color(0XFF2B475E),
-      body: Center(
-        child: SizedBox(
-          width: screenSize.width * 0.9,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/scholar.png',
-              ),
-              const Text(
-                'Scholar Chat',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  color: Colors.white,
-                  fontSize: 30,
-                ),
-              ),
-              SizedBox(height: screenSize.height * 0.06),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'LOGIN',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 26,
+    return GetBuilder<AuthController>(
+      builder: (controller) {
+        return Scaffold(
+          backgroundColor: const Color(0XFF2B475E),
+          body: Center(
+            child: SizedBox(
+              width: screenSize.width * 0.9,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/scholar.png',
                   ),
-                ),
-              ),
-              SizedBox(height: screenSize.height * 0.03),
-              Form(
-                child: Column(
-                  children: [
-                    BoxTextField(
-                      hintText: 'Email',
-                      obscureText: false,
-                      onChanged: (value) {
-                        email = value;
-                      },
+                  const Text(
+                    'Scholar Chat',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      color: Colors.white,
+                      fontSize: 30,
                     ),
-                    SizedBox(height: screenSize.height * 0.01),
-                    BoxTextField(
-                      hintText: 'Password',
-                      obscureText: true,
-                      onChanged: (value) {
-                        password = value;
-                      },
+                  ),
+                  SizedBox(height: screenSize.height * 0.06),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 26,
+                      ),
                     ),
-                    SizedBox(height: screenSize.height * 0.03),
-                    MainButton(
-                      onTap: () {},
-                      title: 'Login',
-                    ),
-                    SizedBox(height: screenSize.height * 0.01),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  SizedBox(height: screenSize.height * 0.03),
+                  Form(
+                    child: Column(
                       children: [
-                        const Text(
-                          'Don\'t have an account?',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pushReplacementNamed(
-                                context, '/register');
+                        BoxTextField(
+                          hintText: 'Email',
+                          obscureText: false,
+                          onChanged: (value) {
+                            controller.emailController.text = value;
                           },
-                          child: const Text(
-                            'Register',
-                            style: TextStyle(
-                              color: Colors.white,
+                        ),
+                        SizedBox(height: screenSize.height * 0.01),
+                        BoxTextField(
+                          hintText: 'Password',
+                          obscureText: true,
+                          onChanged: (value) {
+                            controller.passwordController.text = value;
+                          },
+                        ),
+                        SizedBox(height: screenSize.height * 0.03),
+                        MainButton(
+                          onTap: () {
+                            var auth = FirebaseAuth.instance;
+                            controller.loginWithEmailAndPassword(auth);
+                          },
+                          title: 'Login',
+                        ),
+                        SizedBox(height: screenSize.height * 0.01),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Don\'t have an account?',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
+                            TextButton(
+                              onPressed: () {
+                                Get.offAllNamed(Routes.registerView);
+                              },
+                              child: const Text(
+                                'Register',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      }
     );
   }
 }
--- lib/features/auth/view/register_view.dart
+++ lib/features/auth/view/register_view.dart
@@ -1,110 +1,110 @@
+import 'package:chat_app/features/auth/controller/auth_controller.dart';
 import 'package:chat_app/features/auth/widget/box_text_field.dart';
 import 'package:chat_app/features/auth/widget/main_button.dart';
+import 'package:chat_app/routes/routes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class RegisterView extends StatelessWidget {
   const RegisterView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String? email;
-    String? password;
     Size screenSize = MediaQuery.of(context).size;
-    return Scaffold(
-      backgroundColor: const Color(0XFF2B475E),
-      body: Center(
-        child: SizedBox(
-          width: screenSize.width * 0.9,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/scholar.png',
-              ),
-              const Text(
-                'Scholar Chat',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  color: Colors.white,
-                  fontSize: 30,
-                ),
-              ),
-              SizedBox(height: screenSize.height * 0.06),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'REGISTER',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 26,
+    return GetBuilder<AuthController>(
+      builder: (controller) {
+        return Scaffold(
+          backgroundColor: const Color(0XFF2B475E),
+          body: Center(
+            child: SizedBox(
+              width: screenSize.width * 0.9,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/scholar.png',
                   ),
-                ),
-              ),
-              SizedBox(height: screenSize.height * 0.03),
-              Form(
-                child: Column(
-                  children: [
-                    BoxTextField(
-                      hintText: 'Email',
-                      obscureText: false,
-                      onChanged: (value) {
-                          email = value;
-                      },
+                  const Text(
+                    'Scholar Chat',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      color: Colors.white,
+                      fontSize: 30,
                     ),
-                    SizedBox(height: screenSize.height * 0.01),
-                    BoxTextField(
-                      hintText: 'Password',
-                      obscureText: true,
-                      onChanged: (value) {
-                          password = value;
-                      },
+                  ),
+                  SizedBox(height: screenSize.height * 0.06),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'REGISTER',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 26,
+                      ),
                     ),
-                    SizedBox(height: screenSize.height * 0.03),
-                    MainButton(
-                      onTap: () async {
-                        print(email);
-                        print(password);
-                        var auth = FirebaseAuth.instance;
-                        UserCredential userCredential =
-                            await auth.createUserWithEmailAndPassword(
-                          email: email!,
-                          password: password!,
-                        );
-                        print(userCredential.user!.email);
-                      },
-                      title: 'Register',
-                    ),
-                    SizedBox(height: screenSize.height * 0.01),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  SizedBox(height: screenSize.height * 0.03),
+                  Form(
+                    child: Column(
                       children: [
-                        const Text(
-                          'Already have an account?',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pushReplacementNamed(context, '/login');
+                        BoxTextField(
+                          hintText: 'Email',
+                          obscureText: false,
+                          onChanged: (value) {
+                              controller.emailController.text = value;
                           },
-                          child: const Text(
-                            'Login',
-                            style: TextStyle(
-                              color: Colors.white,
+                        ),
+                        SizedBox(height: screenSize.height * 0.01),
+                        BoxTextField(
+                          hintText: 'Password',
+                          obscureText: true,
+                          onChanged: (value) {
+                              controller.passwordController.text = value;
+                          },
+                        ),
+                        SizedBox(height: screenSize.height * 0.03),
+                        MainButton(
+                          onTap: () async {
+                            print(controller.emailController.text);
+                            print(controller.passwordController);
+                            var auth = FirebaseAuth.instance;
+                            controller.createUserWithEmailAndPassword(auth);
+                          },
+                          title: 'Register',
+                        ),
+                        SizedBox(height: screenSize.height * 0.01),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Already have an account?',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
+                            TextButton(
+                              onPressed: () {
+                                Get.offAllNamed(Routes.loginView);
+                              },
+                              child: const Text(
+                                'Login',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      }
     );
   }
 }
