e9e1bf8fbf1804024c172538f35bc085a4b99f20
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-09-13 22:43:42 +0300

    create auth controller

---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,56 @@
+
+import 'package:chat_app/routes/routes.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController{
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  createUserWithEmailAndPassword(auth) async{
+    try{
+      auth = FirebaseAuth.instance;
+      await auth.createUserWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      Get.offAllNamed(Routes.loginView);
+    } on FirebaseAuthException catch (e){
+      Get.snackbar(
+        'Error',
+        e.message!,
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+    }
+  }
+
+  loginWithEmailAndPassword(auth) async{
+    try{
+      auth = FirebaseAuth.instance;
+      await auth.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      Get.snackbar(
+        'Thanks',
+        'Successfully logged in',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.green,
+        colorText: Colors.white,
+      );
+      //Get.offAllNamed(Routes.homeView);
+    } on FirebaseAuthException catch (e){
+      Get.snackbar(
+        'Error',
+        e.message!,
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+    }
+  }
+
+}
